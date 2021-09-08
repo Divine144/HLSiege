de3e60a6995cb2f62a37b137cc991f1071921e42
JacobMV-BasketDrop <84710151+JacobMV-BasketDrop@users.noreply.github.com>
2021-09-08 16:55:12 +0100

    hopefully added some serialisation to the tile entity

--- src/main/java/com/divinity/hlsiege/common/GoldIdolTileEntity.java
+++ src/main/java/com/divinity/hlsiege/common/GoldIdolTileEntity.java
@@ -52,4 +52,33 @@ public class GoldIdolTileEntity extends TileEntity implements ITickableTileEntit
 		level.setBlock(getBlockPos(),level.getBlockState(getBlockPos()).setValue(GoldIdolBlock.IDOL_STATE,state),3);
 	}
 
+	// ====================================== SERIALISATION ===================================================
+	public void load(BlockState state, CompoundNBT nbt) {
+		super.load(state, nbt);
+		ticks_active = nbt.getInt("active_time");
+		active = nbt.getBoolean("active");
+		warned_near_inactive = nbt.getBoolean("warned");
+
+		ListNBT listnbt = nbt.getList("owners", 10);
+		for(int i = 0; i < listnbt.size(); ++i) {
+			UUID player = listnbt.getCompound(i).getUUID("player");
+			owners.add(player);
+		}
+	}
+
+	public CompoundNBT save(CompoundNBT nbt) {
+		super.save(nbt);
+		nbt.putInt("active_time",ticks_active);
+		nbt.putBoolean("active",active);
+		nbt.putBoolean("warned",warned_near_inactive);
+
+		ListNBT listnbt = new ListNBT();
+		for(UUID player : owners) {
+			CompoundNBT player_nbt = new CompoundNBT();
+			player_nbt.putUUID("player",player);
+			listnbt.add(player_nbt);
+		}
+		nbt.put("owners", listnbt);
+		return save(nbt);
+	}
 }
